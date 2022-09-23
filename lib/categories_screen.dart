0900ebc40e9305841a813c57eaef722a0760e829
Barend Koorzen <koorzenb@gmail.com>
2022-09-23 17:40:55 +0200

    Creating a Grid & Working with Linear Gradients

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import './category_item.dart';
+import './dummy_data.dart';
+
 class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -9,7 +12,9 @@ class MyWidget extends StatelessWidget {
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20),
-      children: [],
+      children: DUMMY_CATEGORIES
+          .map((element) => CategoryItem(element.title, element.color))
+          .toList(),
     );
   }
 }
