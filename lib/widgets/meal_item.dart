bd23795bf6db1f77b02d448cc4b44ee49806acd5
Barend Koorzen <koorzenb@gmail.com>
2022-09-26 12:33:33 +0200

    Finishing the Meal List Item

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -17,6 +17,44 @@ class MealItem extends StatelessWidget {
     @required this.affordability,
   });
 
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return "Simple";
+        break;
+
+      case Complexity.Challenging:
+        return "Challenging";
+        break;
+
+      case Complexity.Hard:
+        return "Hard";
+        break;
+
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return "Affordable";
+        break;
+
+      case Affordability.Pricey:
+        return "Pricey";
+        break;
+
+      case Affordability.Luxurious:
+        return "Luxurious";
+        break;
+
+      default:
+        'Unknown';
+    }
+  }
+
   void _selectMeal() {}
 
   @override
@@ -37,11 +75,70 @@ class MealItem extends StatelessWidget {
                     topLeft: Radius.circular(15),
                     topRight: Radius.circular(15),
                   ),
-                  child: Image.network(imageUrl,
-                      height: 250, width: double.infinity, fit: BoxFit.cover),
+                  child: Image.network(
+                    imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
                 ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                )
               ],
             ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.schedule),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        Text('${duration} min'),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.work),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        Text('${complexityText}'),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.money),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        Text('${affordabilityText}'),
+                      ],
+                    )
+                  ]),
+            )
           ]),
         ));
   }
